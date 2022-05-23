6e16cf18337ce05ff9285f17ce44bc4bdd0f6d46
unknown <maxvill2016@gmail.com>
2022-05-23 17:00:51 +0300

    basic operation edited

--- DataBaseLayer/CRUD/BasicOperation.cs
+++ DataBaseLayer/CRUD/BasicOperation.cs
@@ -24,4 +24,23 @@ public class BasicOperation
     {
         await Task.Run(()=> Add(entity));
     }
+
+    public string ReadAbbriviatureAssociation(string abbriviature)
+    {
+        if (string.IsNullOrEmpty(abbriviature))
+        {
+            throw new ArgumentNullException(nameof(abbriviature));
+        }
+
+
+        CurrencyAssociation result = _context.currencyAssociations?.FirstOrDefault((abbr) => abbr.Abbreviature.Equals(abbriviature));
+
+        if (result is null)
+        {
+            string message = string.Format("abrriviature {0} is not exist", abbriviature);
+            throw new ArgumentException(message);
+        }
+
+        return result.Name;
+    }
 }
