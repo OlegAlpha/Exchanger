5c3d6761c6f4b3e4b923cce5c84750d76d6faf67
Anton Trushchenko <ATrushchenko@ipg.com>
2022-05-24 02:43:44 -0700

    context was added

---
+++ ExchangeService.BusinessLogic/Context/Response.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeService.BusinessLogic.Context;
+internal class Response
+{
+    public string @base { get; set; }
+    string date { get; set; }
+    string start_date { get; set; }
+    string end_date { get; set; }
+    object rates { get; set; }
+    bool success { get; set; } = true;
+    bool fluctuation { get; set; }
+    bool historical { get; set; }
+    object info { get; set; }
+    object query { get; set; }
+
+    string timestamp { get; set; }
+    string result { get; set; }
+}
