d14248a966168bce727d519c7325a8cfb2c0d65f
Anton Trushchenko <ATrushchenko@ipg.com>
2022-05-25 00:33:32 -0700

    get was renamed to post

--- ExchangeService/Controllers/ExchangeController.cs
+++ ExchangeService/Controllers/ExchangeController.cs
@@ -15,35 +15,35 @@ public class ExchangeController : ControllerBase
 
     [HttpGet]
     [Route("exchange")]
-    public async Task<string> Exchange(int userId, decimal amount, string from, string to)
+    public async Task<string> PostExchange(int userId, decimal amount, string from, string to)
     {
         return await _redirect.ExchangeProcess(userId, amount, from, to);
     }
 
     [HttpGet]
     [Route("latest")]
-    public async Task<string?> GetLatestRates(string? @base, string? symbols)
+    public async Task<string?> PostLatestRates(string? @base, string? symbols)
     {
         return await _redirect.LatestRatesProcess(@base, symbols);
     }
 
     [HttpGet]
     [Route("symbols")]
-    public async Task<string?> GetAvailableCurrencies()
+    public async Task<string?> PostAvailableCurrencies()
     {
         return await _redirect.GetAvailableCurrencies();
     }
 
     [HttpGet]
     [Route("timeseries")]
-    public async Task<string?> GetRatesWithin(DateTime endDate, DateTime startDate, string? @base, string? symbols)
+    public async Task<string?> PostRatesWithin(DateTime endDate, DateTime startDate, string? @base, string? symbols)
     {
         return await _redirect.RatesWithinProcess(endDate, startDate, @base, symbols);
     }
 
     [HttpGet]
     [Route("fluctuation")]
-    public async Task<string> Fluctuation(DateTime start, DateTime end, string? baseCurrency, string[]? currencies)
+    public async Task<string> PostFluctuation(DateTime start, DateTime end, string? baseCurrency, string[]? currencies)
     {
         return await _redirect.FluctuationProcessing(start, end, baseCurrency, currencies);
     }
